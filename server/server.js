8641798dddb3e22f73a1fac2a62d8b8e22538e39
Arpon Ganapati <arpon.ganapati7@gmail.com>
2026-09-25 22:43:04 +0600

    Allow Vercel frontend in CORS

--- server/server.js
+++ server/server.js
@@ -11,8 +11,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    "https://arpon-web.vercel.app": true,
+    origin: [
+      "http://localhost:5173",
+      "https://arpon-web.vercel.app",
+    ],
   })
 );
 
